53dd2305575d46d3345c21b0b77e68cf2175824b
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-07-08 12:22:13 +0530

    nav optimize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           window.scrollTo(0,0);
         }}>
           <img src={logo} alt="Logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'> Saiprasad <span className='sm:block hidden'>  Mane</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'> Saiprasad &nbsp; <span className='sm:block hidden'>  Mane</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
