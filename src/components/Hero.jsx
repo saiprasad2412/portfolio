45e683bc606428ce3320d4bc5b4ba829a5fbc43b
Saiprasad Mane <manesaiprasad2412@gmail.com>
2024-07-09 23:30:18 +0530

    Hero sec comp model add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,21 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='realative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+         < div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+         <div className='w-1 sm:h-80 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi , I'm <span className='text-[#915eff]'>Saiprasad Mane</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}> Versatile Full Stack Developer
+          <br className='sm:block hidden'/> | ReactJS, Angular, NodeJS, Express, MongoDB, SQL Expert
+             </p>
+        </div>
+      </div>
+        <ComputersCanvas/>
+    </section>
   )
 }
 
