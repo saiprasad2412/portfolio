dc58b5cc9fdbc89ad76ca3c2d572e54877906763
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-07-10 16:00:53 +0530

    About section completed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -63,7 +63,7 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense >
+      <Suspense  fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
