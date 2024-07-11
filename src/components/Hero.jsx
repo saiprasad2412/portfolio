f155c78c02e4cbd8e0f134958e2ad469be892df5
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-07-11 14:44:52 +0530

    back to a tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,10 +34,10 @@ const Hero = () => {
           // backgroundColor:'red',
           // height:'500px',
         }} 
-        onClick={() => { navigator()
-        }}
         >
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          
+        <a href='#about'>
+        <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -47,13 +47,10 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              onClick={() => navigate('/about')}
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
-        {/* <a href='#about'>
-          
-        </a> */}
+        </a>
       </div>
     </section>
   )
