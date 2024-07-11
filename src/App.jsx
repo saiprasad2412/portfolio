1448ebac434f650aaeab20f17356cd7bd1a0ae16
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-07-11 13:01:39 +0530

    changes for render issue in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import dotenv from 'dotenv'
 import Footer from "./components/Footer.jsx";
+import RenderIfVisible from 'react-render-if-visible'
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
+          <RenderIfVisible>
           <Hero />
+      </RenderIfVisible>
         </div>
         <About />
         <Experience />
+        <RenderIfVisible>
         <Tech />
+      </RenderIfVisible>
         <Works />
         {/* <Feedbacks /> */}
         <div className='relative z-0'>
